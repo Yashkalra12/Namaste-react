474b984dff9a89f025aad74c39e26b61a45901a2
Yashkalra12 <yash1578.be21@chitkara.edu.in>
2024-01-27 18:55:26 +0530

    Episode-2

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const heading = React.createElement("h1", { id: "heading" }, "hello from react");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
