8f0f53357ec2795c0920c4e6a08070050ec6f11c
Yashkalra12 <yash1578.be21@chitkara.edu.in>
2024-01-28 17:00:00 +0530

    Episode-3

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const heading = React.createElement("h1", { id: "heading" }, "hello from react");
+const span= (<h1>Span Eelement</h1>);
+
+const Title= () =>(<h1 id="title">Namaste React in Jsx</h1>);
+
+const body= (<h1 > {span} Body React Eelement in Jsx</h1>);
+
+
+const HeadingComponent = () => (
+<div> 
+{Title()} {/*can be called as a function too */}
+<Title></Title>
+<Title />  //This is ComponentCompo sition
+{body}
+<h1 id="heading">Namaste React in React Component</h1>
+</div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent/>);
